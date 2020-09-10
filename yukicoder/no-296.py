a3473fbcf135c165052f1cec5aa7d8f4bb7813cf
Inseo Kim <sano307@naver.com>
2020-09-10 16:04:47 +0900

    Update 2020-09-10

--- yukicoder/no-296.py
+++ yukicoder/no-296.py
@@ -1,18 +1,13 @@
 HOUR_TO_MINUTES = 60
-DAY_TO_MINUTES = 60 * 24
 
 sleep_count, alarm_hours, alarm_minutes, alarm_cycle = map(int, input().split())
 
-convert_alarm_minutes = alarm_hours * 60
+convert_alarm_minutes = alarm_hours * HOUR_TO_MINUTES
 convert_alarm_minutes += alarm_minutes
+convert_alarm_minutes += alarm_cycle * (sleep_count - 1)
 
-if sleep_count > 1:
-    for i in range(1, sleep_count):
-        convert_alarm_minutes += alarm_cycle
+wakeup_hour = convert_alarm_minutes // HOUR_TO_MINUTES
+wakeup_minutes = convert_alarm_minutes - wakeup_hour * HOUR_TO_MINUTES
 
-        if convert_alarm_minutes >= DAY_TO_MINUTES:
-            convert_alarm_minutes -= DAY_TO_MINUTES
-
-wakeup_hour, wakeup_minutes = divmod(convert_alarm_minutes, HOUR_TO_MINUTES)
-print(wakeup_hour)
+print(wakeup_hour % 24)
 print(wakeup_minutes)
---
+++ yukicoder/no-69.py
@@ -0,0 +1,7 @@
+base = str(input())
+diff = str(input())
+
+if sorted(base) == sorted(diff):
+    print('YES')
+else:
+    print('NO')
